4cfd8b57df68cb9556b7b7c7b3a19d27e7d8b10f
Roop Ghosh <roopkumar@me.com>
2018-01-20 22:22:37 -0500

    remove db connectivity for now

--- assignment/models/model.server.js
+++ assignment/models/model.server.js
@@ -1,9 +1,9 @@
 module.exports = function () {
 
    
-    var mongoose = require('mongoose');
+    //var mongoose = require('mongoose');
     var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL|| 'mongodb://127.0.0.1:27017/wam-fall-2016';
-    mongoose.connect(connectionString);
+    //mongoose.connect(connectionString);
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
     var pageModel= require("./page/page.model.server")();
